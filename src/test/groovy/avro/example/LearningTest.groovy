64a49a3030ca70d208070eff0259131adbda6678
Ron Kurr <kurr@kurron.org>
2015-05-06 16:30:59 -0400

    Got a working sample.

---
+++ src/test/groovy/avro/example/LearningTest.groovy
@@ -0,0 +1,58 @@
+package avro.example
+
+import org.apache.avro.Schema
+import org.apache.avro.file.DataFileReader
+import org.apache.avro.file.DataFileWriter
+import org.apache.avro.generic.GenericData
+import org.apache.avro.generic.GenericDatumReader
+import org.apache.avro.generic.GenericDatumWriter
+import org.apache.avro.generic.GenericRecord
+import org.apache.avro.io.DatumReader
+import org.apache.avro.io.DatumWriter
+import spock.lang.Specification
+
+/**
+ * A learning test of the Avro library.
+ */
+class LearningTest extends Specification {
+
+    def 'exercise codec'() {
+
+        given: 'a parsed schema'
+        def schema = new Schema.Parser().parse( getClass().classLoader.getResourceAsStream( 'user.avsc' ) )
+
+        and: 'some records'
+        GenericRecord user1 = new GenericData.Record( schema )
+        user1.put( 'name', 'Alyssa' )
+        user1.put( 'favorite_number', 256)
+        // Leave favorite color null
+
+        GenericRecord user2 = new GenericData.Record( schema )
+        user2.put( 'name', 'Ben' )
+        user2.put( 'favorite_number', 7 )
+        user2.put( 'favorite_color', 'red' )
+
+        and: 'encoded records to disk'
+        File file = new File( 'build/users.avro' )
+        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<GenericRecord>( schema )
+        DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>( datumWriter )
+        dataFileWriter.create( schema, file )
+        dataFileWriter.append( user1 )
+        dataFileWriter.append( user2 )
+        dataFileWriter.close()
+
+        when: 'the records are decoded'
+        DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>( schema )
+        DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>( file, datumReader )
+        GenericRecord user = null;
+        while ( dataFileReader.hasNext() ) {
+            // Reuse user object by passing it to next(). This saves us from
+            // allocating and garbage collecting many objects for files with
+            // many items.
+            user = dataFileReader.next( user )
+            System.out.println( user )
+        }
+
+        then: 'something'
+    }
+}
