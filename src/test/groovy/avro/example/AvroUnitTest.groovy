0c8fde78d23c5d2cd42623d826030c671c8808b5
Ron Kurr <kurr@kurron.org>
2017-06-15 20:42:13 -0400

    First schema is working.

--- src/test/groovy/avro/example/AvroUnitTest.groovy
+++ src/test/groovy/avro/example/AvroUnitTest.groovy
@@ -2,8 +2,9 @@ package avro.example
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.avro.AvroFactory
-import com.fasterxml.jackson.dataformat.avro.schema.AvroSchemaGenerator
+import com.fasterxml.jackson.dataformat.avro.AvroSchema
 import groovy.transform.Canonical
+import org.apache.avro.Schema
 import spock.lang.Specification
 
 import java.util.concurrent.ThreadLocalRandom
@@ -14,39 +15,27 @@ import java.util.concurrent.ThreadLocalRandom
 class AvroUnitTest extends Specification {
 
     @Canonical
-    static class Payload {
-        boolean processed
-        String text
-        byte[] bytes
+    static class User100 {
+        String name
     }
 
     void 'exercise Jackson Avro support'() {
 
         given: 'an Avro schema and mapper'
+        def schemaStream = AvroUnitTest.class.classLoader.getResourceAsStream( 'schemas/user-1.0.0.json' )
+        def raw = new Schema.Parser().setValidate( true ).parse( schemaStream )
+        def schema = new AvroSchema( raw )
         def mapper = new ObjectMapper( new AvroFactory() )
-        def generator = new AvroSchemaGenerator()
-        mapper.acceptJsonFormatVisitor( Payload.class, generator )
-        def schema = generator.getGeneratedSchema()
 
         and: 'an Avro encoded instance'
-        def text = Integer.toHexString( ThreadLocalRandom.current().nextInt( 0, Integer.MAX_VALUE ) )
-        byte[] bytes = new byte[8]
-        ThreadLocalRandom.current().nextBytes( bytes )
-        def original = new Payload( text: text, bytes: bytes )
-        byte[] encodedOriginal = mapper.writer( schema ).writeValueAsBytes( original )
-
-        and: 'an Avro encoded decoy instance'
-        def decoy = new Payload( text: 'decoy', bytes: 'decoy'.utf8Bytes )
-        byte[] encodedDecoy = mapper.writer( schema ).writeValueAsBytes( decoy )
+        def name = Integer.toHexString( ThreadLocalRandom.current().nextInt( 0, Integer.MAX_VALUE ) )
+        def original = new User100( name: name )
+        byte[] encoded = mapper.writer( schema ).writeValueAsBytes( original )
 
         when: 'the instance are recreated'
-        Payload decodedDecoy = mapper.readerFor( Payload.class ).with( schema ).readValue( encodedDecoy )
-        Payload decodedOriginal = mapper.readerFor( Payload.class ).with( schema ).readValue( encodedOriginal )
+        User100 decoded = mapper.readerFor( User100.class ).with( schema ).readValue( encoded )
 
-        then: 'the decoded decoy does not match the original'
-        decodedDecoy != original
-
-        and: 'the decoded original does match the original'
-        decodedOriginal == original
+        then: 'encoded and decoded match'
+        decoded == original
     }
 }
