33784eea927ad9ad4cc9de03fc4a77e665f88996
Ron Kurr <kurr@kurron.org>
2017-06-19 19:05:05 -0400

    Switch to sub-project setup

--- generate-files-from-schema.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-AVRO_JAR=${1:-avro-tools-1.8.2.jar}
-
-for schema in user-1.0.0.json user-1.1.0.json user-1.2.0.json; do
-  CMD="java -jar ${AVRO_JAR} compile schema src/test/resources/schemas/${schema} src/test/${schema}"
-  echo ${CMD}
-  ${CMD}
-done
