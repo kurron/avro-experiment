c5fd75d70ea35f13ef2e64f479aaeeffa048f6bd
Ron Kurr <kurr@kurron.org>
2017-06-19 18:38:07 -0400

    Save everything before switching to a multi-project build.

--- generate-files-from-schema.sh
+++ generate-files-from-schema.sh
@@ -2,6 +2,8 @@
 
 AVRO_JAR=${1:-avro-tools-1.8.2.jar}
 
-CMD="java -jar ${AVRO_JAR} compile schema src/test/resources/schemas/*.json src/test/java"
-echo ${CMD}
-${CMD}
+for schema in user-1.0.0.json user-1.1.0.json user-1.2.0.json; do
+  CMD="java -jar ${AVRO_JAR} compile schema src/test/resources/schemas/${schema} src/test/${schema}"
+  echo ${CMD}
+  ${CMD}
+done
