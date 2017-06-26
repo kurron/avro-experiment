7e8d8b401632e6d064721d269018c14d20cb1cdf
Ron Kurr <kurr@kurron.org>
2017-06-25 20:11:19 -0400

    Split out the writing of the data into its own application.

---
+++ v100/src/main/groovy/org/kurron/avro/example/DatFileWriter.groovy
@@ -0,0 +1,26 @@
+package org.kurron.avro.example
+
+import org.apache.avro.file.DataFileWriter
+import org.apache.avro.specific.SpecificDatumWriter
+
+/**
+ * Should run before the tests, outputting this schema's data to a file.
+ */
+class DatFileWriter {
+
+    static final dataFileLocation = '../v100.bin'
+    static final String NAME = 'name-v100'
+
+    static void main(String[] args) {
+
+        def encoded = User.newBuilder().setName(NAME).build()
+
+        def datumWriter = new SpecificDatumWriter<User>( User )
+        def dataFileWriter = new DataFileWriter<User>( datumWriter )
+
+        dataFileWriter.create( encoded.getSchema(), new File( dataFileLocation ) )
+        dataFileWriter.append( encoded )
+        dataFileWriter.flush()
+        dataFileWriter.close()
+    }
+}
--- v100/src/test/groovy/org/kurron/avro/example/AvroIntegrationTest.groovy
+++ v100/src/test/groovy/org/kurron/avro/example/AvroIntegrationTest.groovy
@@ -1,9 +1,7 @@
 package org.kurron.avro.example
 
 import org.apache.avro.file.DataFileReader
-import org.apache.avro.file.DataFileWriter
 import org.apache.avro.specific.SpecificDatumReader
-import org.apache.avro.specific.SpecificDatumWriter
 import spock.lang.Specification
 
 /**
@@ -15,24 +13,14 @@ class AvroIntegrationTest extends Specification {
 
     def 'exercise codec'() {
         given: 'a fresh object'
-        def encoded = User.newBuilder().setName( 'name-v100' ).build()
-
-        and: 'a writer'
-        def datumWriter = new SpecificDatumWriter<User>( User )
-        def dataFileWriter = new DataFileWriter<User>( datumWriter )
-
-        and: 'an object is encoded to disk'
-        dataFileWriter.create( encoded.getSchema(), new File( dataFileLocation ) )
-        dataFileWriter.append( encoded )
-        dataFileWriter.flush()
-        dataFileWriter.close()
+        def dataFile = new File(dataFileLocation)
 
         when: 'the object is decoded from disk'
         def userDatumReader = new SpecificDatumReader<User>(User)
-        def dataFileReader = new DataFileReader<User>(new File(dataFileLocation), userDatumReader)
+        def dataFileReader = new DataFileReader<User>(dataFile, userDatumReader)
         def decoded = dataFileReader.hasNext() ? dataFileReader.next( new User() ) : new User()
 
         then: 'the encoded and decoded match'
-        encoded.name == decoded.name as String
+        DatFileWriter.NAME == decoded.name as String
     }
 }
