a9732b710ff41f92fc0dd0567aa1a7a4b5d7afb2
Ron Kurr <kurr@kurron.org>
2017-07-02 12:58:32 -0400

    Split out the writing of the data into its own application.

--- v200/src/test/groovy/org/kurron/avro/example/AvroIntegrationTest.groovy
+++ v200/src/test/groovy/org/kurron/avro/example/AvroIntegrationTest.groovy
@@ -2,12 +2,9 @@ package org.kurron.avro.example
 
 import org.apache.avro.AvroTypeException
 import org.apache.avro.file.DataFileReader
-import org.apache.avro.file.DataFileWriter
 import org.apache.avro.specific.SpecificDatumReader
-import org.apache.avro.specific.SpecificDatumWriter
+import org.apache.avro.util.Utf8
 import spock.lang.Specification
-
-import java.time.LocalDateTime
 
 /**
  * Exercises Avro codec.
@@ -15,53 +12,25 @@ import java.time.LocalDateTime
 class AvroIntegrationTest extends Specification {
 
     static final previousVersionDataFileLocation = '../v140.bin'
-    static final dataFileLocation = '../v200.bin'
-    static final now = LocalDateTime.now()
-    static final date = now.toLocalDate().toEpochDay() as int
-    static final time = now.toLocalTime().toSecondOfDay() * 1000
 
     def 'exercise codec'() {
-        given: 'a fresh object'
-        def encoded = User.newBuilder().setFirstname( 'firstname-v200' )
-                                       .setLastname( 'lastname-v200' )
-                                       .setUsername( 'username-v200' )
-                                       .setActive( true )
-                                       .setId( Integer.MAX_VALUE )
-                                       .setAddedDate( date )
-                                       .setAddedTime( time )
-                                       .setGender( Gender.FEMALE )
-                                       .setBreakingChange( 'breakingChange-v200' )
-                                       .build()
-        encoded.comments.add( 'Reset password v200' )
-        encoded.sessions['May'] = 200
-
-        and: 'a writer'
-        def datumWriter = new SpecificDatumWriter<User>( User )
-        def dataFileWriter = new DataFileWriter<User>( datumWriter )
-
-        and: 'an object is encoded to disk'
-        dataFileWriter.create( encoded.getSchema(), new File( dataFileLocation ) )
-        dataFileWriter.append( encoded )
-        dataFileWriter.flush()
-        dataFileWriter.close()
-
         when: 'the object is decoded from disk'
         def userDatumReader = new SpecificDatumReader<User>(User)
-        def dataFileReader = new DataFileReader<User>(new File(dataFileLocation), userDatumReader)
+        def dataFileReader = new DataFileReader<User>(new File(DatFileWriter.dataFileLocation), userDatumReader)
         def decoded = dataFileReader.hasNext() ? dataFileReader.next( new User() ) : new User()
 
         then: 'the encoded and decoded match'
-        encoded.firstname == decoded.firstname as String
-        encoded.lastname == decoded.lastname as String
-        encoded.username == decoded.username as String
-        encoded.active == decoded.active
-        encoded.id == decoded.id
-        encoded.addedDate == decoded.addedDate
-        encoded.addedTime == decoded.addedTime
-        encoded.gender == decoded.gender
-        encoded.comments == decoded.comments
+        DatFileWriter.FIRST_NAME == decoded.firstname as String
+        DatFileWriter.LAST_NAME == decoded.lastname as String
+        DatFileWriter.USER_NAME == decoded.username as String
+        DatFileWriter.ACTIVE == decoded.active
+        DatFileWriter.ID == decoded.id
+        DatFileWriter.date == decoded.addedDate
+        DatFileWriter.time == decoded.addedTime
+        DatFileWriter.GENDER == decoded.gender
+        'Reset password v200' == decoded.comments.first() as String
         // there is a CharSet to String comparison issue with the keys
-        encoded.sessions as String == decoded.sessions as String
+        200 == decoded.sessions[ new Utf8('May') ]
     }
 
     def 'exercise forwards compatibility'() {
